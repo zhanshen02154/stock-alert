52e7214fc7375ac83e67fb27e3f487a1de5e8b82
wangjiawei1995 <zhanshen02154@gmail.com>
2026-04-27 21:32:20 +0800

    feat: 新增RAG信息检索查询 (#28)

--- src/service/chat.py
+++ src/service/chat.py
@@ -1,8 +1,14 @@
+import logging
 import uuid
 from collections.abc import AsyncGenerator
 from dataclasses import dataclass
+
 from src.agents.inventory_agent import InventoryAgent
 from src.repository.session import SessionRepository
+from src.utils.plain_text import sanitize_text
+
+logger = logging.getLogger(__name__)
+
 
 @dataclass
 class SSEMessage:
@@ -11,16 +17,25 @@ class SSEMessage:
     message_id: str = ""
     full_content: str = ""
 
+
 class ChatService:
-    def __init__(self, agent: InventoryAgent, session_repo: SessionRepository):
+    def __init__(
+        self,
+        agent: InventoryAgent,
+        session_repo: SessionRepository,
+    ):
         self.__agent = agent
         self.__session_repo = session_repo
 
     def add_message(self, message: str, session_id: str, user_id: int) -> int:
-        session_info = self.__session_repo.find_session_by_id(session_id=session_id, user_id=user_id)
+        session_info = self.__session_repo.find_session_by_id(
+            session_id=session_id, user_id=user_id
+        )
         if not session_info:
             raise ValueError(f"会话不存在: {session_id}")
-        msg_id = self.__session_repo.save_message(session_id=session_id, role="user", content=message)
+        msg_id = self.__session_repo.save_message(
+            session_id=session_id, role="user", content=message
+        )
         return msg_id
 
     async def chat_astream(self, session_id: str) -> AsyncGenerator[SSEMessage]:
@@ -29,18 +44,55 @@ class ChatService:
         :param session_id: 会话ID
         :return: 异步生成器
         """
+        message_id = str(uuid.uuid4())
+        final_msg = ""
         try:
-            msg_info = self.__session_repo.find_session_last_message(session_id=session_id)
+            msg_info = self.__session_repo.find_session_last_message(
+                session_id=session_id
+            )
             full_content = []
-            message_id = str(uuid.uuid4())
+            user_message = sanitize_text(text=msg_info["content"])
 
-            async for chunk in self.__agent.astream(message=msg_info['content'], thread_id=session_id):
-                full_content.append(chunk['text'])  # 追加全部内容
-                yield SSEMessage(type="chunk", content=chunk['text'], full_content="".join(full_content), message_id=message_id)
+            async for chunk in self.__agent.rag_astream(message=user_message):
+                full_content.append(chunk["text"])
+                yield SSEMessage(
+                    type="chunk",
+                    content=chunk["text"],
+                    full_content="".join(full_content),
+                    message_id=message_id,
+                )
+            # if self.__agent._is_rag_query(user_message):
+            #     async for chunk in self.__agent.rag_astream(message=user_message):
+            #         full_content.append(chunk['text'])
+            #         yield SSEMessage(
+            #             type="chunk",
+            #             content=chunk['text'],
+            #             full_content="".join(full_content),
+            #             message_id=message_id,
+            #         )
+            # else:
+            #     async for chunk in self.__agent.astream(message=user_message, thread_id=session_id):
+            #         if chunk.get('type') == 'text':
+            #             full_content.append(chunk['text'])
+            #             yield SSEMessage(
+            #                 type="chunk",
+            #                 content=chunk['text'],
+            #                 full_content="".join(full_content),
+            #                 message_id=message_id,
+            #             )
 
             final_msg = "".join(full_content)
-            self.__session_repo.save_message(session_id=session_id, role="assistant", content=final_msg)
-
-            yield SSEMessage(type="done", content=final_msg, full_content=final_msg, message_id=message_id)
+            yield SSEMessage(
+                type="done",
+                content=final_msg,
+                full_content=final_msg,
+                message_id=message_id,
+            )
         except Exception as e:
-            yield SSEMessage(type="error", content=str(e))
+            logger.error(e)
+            final_msg = str(e)
+            yield SSEMessage(type="error", content=str(e), message_id=message_id)
+        finally:
+            self.__session_repo.save_message(
+                session_id=session_id, role="assistant", content=final_msg
+            )
